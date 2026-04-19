4da9b42d5bbf9db16291adc26d0d64afd4c8ce8f
SoSNate <12natanel@gmail.com>
2026-04-19 16:23:25 +0300

    Set base path for GitHub Pages
    Added base path for GitHub Pages deployment.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
+  // הוספנו את השורה הזו כדי שגיטהאב פייג'ס ימצא את הקבצים
+  base: '/Leittner-Quest/', 
+  
   plugins: [react()],
 
   build: {
